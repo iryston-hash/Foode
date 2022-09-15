870893936bab4c76a65262b4f217d08b94dacfde
iryston <iryston58@gmail.com>
2022-09-16 01:12:55 +0200

    Pagination , TODO listeners

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -3,40 +3,56 @@ import icons from 'url:../../img/icons.svg';
 
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
+
   _generateMarkup() {
+    const currentPage = this._data.page;
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPagination
     );
     console.log(numPages);
     // Page 1 , ...
-    if (this._data.page === 1 && numPages > 1) {
-      return `page 1, others`;
+    if (currentPage === 1 && numPages > 1) {
+      return `
+        <button class="btn--inline pagination__btn--next">
+          <span>${currentPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
     }
 
     // last page
-    if (this._data.page === numPages && numPages > 1) {
+    if (currentPage === numPages && numPages > 1) {
       return ` 
-      <button class="btn--inline pagination__btn--prev">
-        <svg class="search__icon">
-          <use href="src/img/icons.svg#icon-arrow-left"></use>
-        </svg>
-        <span>Page 1</span>
-      </button>
+        <button class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>${currentPage - 1}</span>
+        </button>
       `;
     }
     // Other pages
-    if (this._data.page < numPages) {
-      return `other page`;
+    if (currentPage < numPages) {
+      return `
+        <button class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>${currentPage - 1}</span>
+        </button>
+
+        <button class="btn--inline pagination__btn--next">
+          <span>${currentPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
     }
     // Page 1 , others void
-    return `only 1`;
+    return ``;
   }
 }
 export default new PaginationView();
-
-// <button class="btn--inline pagination__btn--next">
-//   <span>Page 3</span>
-//   <svg class="search__icon">
-//     <use href="src/img/icons.svg#icon-arrow-right"></use>
-//   </svg>
-// </button>
